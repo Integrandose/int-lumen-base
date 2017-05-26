1a40d9c9094fbc05adda81b57c0eb74b96c8d612
Renato S Moreno <renatomorenocz@gmail.com>
2017-05-26 13:21:02 -0300

    App suport for Field Lists

---
+++ src/Transformer/ApiTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Int\Lumen\Core\Transformer;
+
+
+use League\Fractal\TransformerAbstract;
+
+class ApiTransformer extends TransformerAbstract
+{
+
+    protected function filterFields($fields, $type) {
+
+        $request = app('request');
+        $filterFields = $request->get('fields');
+
+        if (!isset($filterFields[$type]) ) {
+            return $fields;
+        }
+
+        $listFields = explode(',', $filterFields[$type]);
+
+        return collect($fields)->only($listFields)->toArray();
+    }
+
+}
